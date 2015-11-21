388eb25d04f2ab979d3606f2bcb6b5835ce33257
yuanqing <desk@yuanqing.sg>
2015-11-21 21:59:12 +0800

    Refactor; add `middleware`

--- example/index.js
+++ example/index.js
@@ -1,34 +1,44 @@
 var Fleur = require('..');
 
 var Foo = Fleur.actions('foo', {
-  synchronous: function(state) {
-    return Fleur.assign(state, {
-      status: 'pending'
-    });
-  },
-  asynchronous: function(state, dispatch) {
-    dispatch(Foo.synchronous());
+  request: function(state, dispatch) {
+    dispatch(Foo.requestPending());
     return Fleur.promise(function(resolve) {
       setTimeout(function() {
-        resolve(Fleur.assign(state, {
-          items: [
-            { title: 'foo', id: 1 },
-            { title: 'bar', id: 2 },
-            { title: 'baz', id: 3 }
-          ],
-          status: 'success'
-        }));
-      }, 100);
+        dispatch(Foo.requestSuccess([
+          { title: 'foo', id: 1 },
+          { title: 'bar', id: 2 },
+          { title: 'baz', id: 3 }
+        ])).then(resolve);
+      }, 200);
+    });
+  },
+  requestPending: function(state) {
+    return Fleur.assign(state, {
+      pending: true
+    });
+  },
+  requestSuccess: function(state, dispatch, items) {
+    return Fleur.assign(state, {
+      items: items,
+      pending: false
     });
   }
 });
 
-var log = function(object) {
-  console.log(JSON.stringify(object, null, 2));
+var logger = function(state, dispatch) {
+  return function(action) {
+    console.log('ACTION:', action.key, action.actionName);
+    var result = dispatch(action);
+    console.log('STATE:', state);
+    return result;
+  };
 };
 
-var store = Fleur.store();
-store.dispatch(Foo.asynchronous())
+var store = Fleur.store({
+  middleware: [logger]
+});
+store.dispatch(Foo.request())
   .then(function() {
-    log(store.getState());
+    console.log(store.getState());
   });
--- test/store.js
+++ test/store.js
@@ -1,4 +1,6 @@
 var test = require('tape');
+var cloneDeep = require('lodash.clonedeep');
+
 var Fleur = require('..');
 
 test('without `initialState`', function(t) {
@@ -23,11 +25,40 @@ test('with `initialState`', function(t) {
   t.notEqual(state, initialState);
 });
 
-test('dispatch a synchronous `action`, without `listener`', function(t) {
-  t.plan(4);
+test('dispatch a synchronous `action`, without a `listener`', function(t) {
+  t.plan(5);
   var store = Fleur.store();
-  var action = function(state) {
+  var action = function(state, dispatch) {
     t.equal(arguments.length, 2);
+    t.equal(dispatch, store.dispatch);
+    t.looseEqual(state, {});
+    return {
+      bar: 'changed'
+    };
+  };
+  action.key = 'foo';
+  t.looseEqual(store.getState(), {});
+  store.dispatch(action);
+  t.looseEqual(store.getState(), {
+    foo: {
+      bar: 'changed'
+    }
+  });
+});
+
+test('dispatch a synchronous `action`, with middleware', function(t) {
+  t.plan(4);
+  var store = Fleur.store({
+    middleware: [
+      function(state, dispatch) {
+        return function(action) {
+          t.looseEqual(state, {});
+          return dispatch(action);
+        };
+      }
+    ]
+  });
+  var action = function(state) {
     t.looseEqual(state, {});
     return {
       bar: 'changed'
@@ -44,7 +75,7 @@ test('dispatch a synchronous `action`, without `listener`', function(t) {
 });
 
 test('dispatch a synchronous `action`, with `state[key]` being initially `undefined`', function(t) {
-  t.plan(7);
+  t.plan(6);
   var store = Fleur.store({
     listener: function(state) {
       t.equal(arguments.length, 1);
@@ -57,7 +88,6 @@ test('dispatch a synchronous `action`, with `state[key]` being initially `undefi
     }
   });
   var action = function(state) {
-    t.equal(arguments.length, 2);
     t.looseEqual(state, {});
     return {
       bar: 'changed'
@@ -74,7 +104,7 @@ test('dispatch a synchronous `action`, with `state[key]` being initially `undefi
 });
 
 test('dispatch a synchronous `action`, with `state[key]` having an initial value', function(t) {
-  t.plan(7);
+  t.plan(4);
   var initialState = {
     foo: {
       bar: 'initial'
@@ -83,8 +113,6 @@ test('dispatch a synchronous `action`, with `state[key]` having an initial value
   var store = Fleur.store({
     initialState: initialState,
     listener: function(state) {
-      t.equal(arguments.length, 1);
-      t.equal(state, store.getState());
       t.looseEqual(state, {
         foo: {
           bar: 'changed'
@@ -93,7 +121,6 @@ test('dispatch a synchronous `action`, with `state[key]` having an initial value
     }
   });
   var action = function(state) {
-    t.equal(arguments.length, 2);
     t.looseEqual(state, {
       bar: 'initial'
     });
@@ -115,8 +142,8 @@ test('dispatch a synchronous `action`, with `state[key]` having an initial value
   });
 });
 
-test('dispatch an asynchronous `action`', function(t) {
-  t.plan(7);
+test('dispatch an asynchronous `action`, invoking `resolve` with the new `state`', function(t) {
+  t.plan(4);
   var initialState = {
     foo: {
       bar: 'initial'
@@ -125,8 +152,6 @@ test('dispatch an asynchronous `action`', function(t) {
   var store = Fleur.store({
     initialState: initialState,
     listener: function(state) {
-      t.equal(arguments.length, 1);
-      t.equal(state, store.getState());
       t.looseEqual(state, {
         foo: {
           bar: 'changed'
@@ -135,7 +160,112 @@ test('dispatch an asynchronous `action`', function(t) {
     }
   });
   var action = function(state) {
-    t.equal(arguments.length, 2);
+    t.looseEqual(state, {
+      bar: 'initial'
+    });
+    return Fleur.promise(function(resolve) {
+      setTimeout(function() {
+        resolve({
+          bar: 'changed'
+        });
+      }, 0);
+    });
+  };
+  action.key = 'foo';
+  t.looseEqual(store.getState(), {
+    foo: {
+      bar: 'initial'
+    }
+  });
+  store.dispatch(action).then(function() {
+    t.looseEqual(store.getState(), {
+      foo: {
+        bar: 'changed'
+      }
+    });
+  });
+});
+
+test('dispatch an asynchronous `action` that itself dispatches a synchronous action', function(t) {
+  t.plan(6);
+  var initialState = {
+    foo: {
+      bar: 'initial'
+    }
+  };
+  var states = [];
+  var store = Fleur.store({
+    initialState: initialState,
+    listener: function(state) {
+      states.push(cloneDeep(state));
+    }
+  });
+  var pendingAction = function(state) {
+    t.looseEqual(state, {
+      bar: 'initial'
+    });
+    return {
+      bar: 'pending'
+    };
+  };
+  pendingAction.key = 'foo';
+  var successAction = function(state) {
+    t.looseEqual(state, {
+      bar: 'pending'
+    });
+    return {
+      bar: 'success'
+    };
+  };
+  successAction.key = 'foo';
+  var asynchronousAction = function(state, dispatch) {
+    t.looseEqual(state, {
+      bar: 'initial'
+    });
+    dispatch(pendingAction);
+    return Fleur.promise(function(resolve) {
+      setTimeout(function() {
+        dispatch(successAction).then(resolve);
+      }, 0);
+    });
+  };
+  asynchronousAction.key = 'foo';
+  t.looseEqual(store.getState(), {
+    foo: {
+      bar: 'initial'
+    }
+  });
+  store.dispatch(asynchronousAction).then(function() {
+    t.looseEqual(store.getState(), {
+      foo: {
+        bar: 'success'
+      }
+    });
+    t.looseEqual(states, [
+      { foo: { bar: 'pending' } },
+      { foo: { bar: 'success' } }
+    ]);
+  });
+});
+
+test('dispatch an asynchronous `action`, invoking `resolve` with the new `state`', function(t) {
+  t.plan(4);
+  var initialState = {
+    foo: {
+      bar: 'initial'
+    }
+  };
+  var store = Fleur.store({
+    initialState: initialState,
+    listener: function(state) {
+      t.looseEqual(state, {
+        foo: {
+          bar: 'changed'
+        }
+      });
+    }
+  });
+  var action = function(state) {
     t.looseEqual(state, {
       bar: 'initial'
     });
